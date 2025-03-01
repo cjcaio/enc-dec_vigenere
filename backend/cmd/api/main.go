93148457646e6bc738dcb5bafa5d062c5d0e8c32
Caio Jorge <caiojqueiroz@gmail.com>
2025-03-01 12:00:57 -0400

    Refactor main to initialize and start HTTP server
    Replaces cipher logic with a server initialization workflow using the `server` package. Configures routes and starts the server on port 8080 for better modularity and separation of concerns.

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/cjcaio/enc-dec_vigenere/internal/cipher"
+	"github.com/cjcaio/enc-dec_vigenere/internal/server"
+	"log"
 )
 
 func main() {
-	vigenere := cipher.NewVigenere()
+	srv := server.NewServer()
+	srv.ConfigureRoutes()
 
-	message := "First Message"
-	key := "key"
-
-	encrypted := vigenere.Encrypt(message, key)
-	decrypted := vigenere.Decrypt(encrypted, key)
-
-	fmt.Println("Original message:", message)
-	fmt.Println("\nEncrypted message:", encrypted, "\nUsing key:", key, "\n")
-	fmt.Println("Decrypted message:", decrypted)
+	log.Fatal(srv.Start(":8080"))
 }
